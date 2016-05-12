010af60d3f668b9d8f2b68c22e5628711ca934d6
Mac <mac@macbook-pro.local>
2016-05-12 21:33:08 +0800

    asd

---
+++ GitDemo/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by Mac on 16/5/11.
+//  Copyright © 2016年 iBoKan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property(nonatomic) int sum;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    int a = 5;
+    int b = 10;
+    self.sum = a + b;
+    NSLog(@"The result is :%d",self.sum);
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
